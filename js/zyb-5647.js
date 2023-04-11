4758e778fd097352635d64c28ec8f95b81980263
GabrielMachadoCP <rm99880@fiap.com.br>
2023-04-11 11:50:40 -0300

    Trabalhando com forEach

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -13,6 +13,15 @@ const liElement = document.getElementsByTagName("li");
 //Declaração de ARRAYS
 let nr1 = [1,2,3,4,5];
 let nr2 = [6,7,8,9,10];
+//let nr3 = nr1 + nr2;
+let nr3 = [nr1,nr2];
 
-console.log(`Array-1" ${nr1}`);
-console.log(`Array-2" ${nr2}`);
+console.log(`Array-1 ${nr1}`);
+console.log(`Array-2 ${nr2}`);
+console.log(`Array-3 ${nr3}`);
+
+
+//Listando um array! Técnica especial
+nr3.forEach((itemDoArray)=> {
+    console.log(`Array-3 sendo listado ${itemDoArray}`);
+})  
