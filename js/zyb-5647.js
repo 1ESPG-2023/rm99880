a696e8125bc0fc7c28d1fa0a19f2e07c310d1a63
GabrielMachadoCP <rm99880@fiap.com.br>
2023-04-11 11:44:19 -0300

    Trabalhando com arrays e HTMLcollections

---
+++ js/zyb-5647.js
@@ -0,0 +1,18 @@
+const liElement = document.getElementsByTagName("li");
+
+// for (let index = 0; index < liElement.length; index++) {
+//     console.log(liElement[index].textContent)
+    
+// }
+// console.log(liElement.length)
+
+//Objetos são formados por propriedades (innerHTML - formata tags) (innerText - formata textos)
+
+
+
+//Declaração de ARRAYS
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+
+console.log(`Array-1" ${nr1}`);
+console.log(`Array-2" ${nr2}`);
