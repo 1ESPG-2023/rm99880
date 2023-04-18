06dc979780ff253f2a41df82be16f2bce29ffef1
GabrielMachadoCP <rm99880@fiap.com.br>
2023-04-18 10:54:34 -0300

    html collection e arrays

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,4 +1,4 @@
-const liElement = document.getElementsByTagName("li");
+// const liElement = document.getElementsByTagName("li");
 
 // for (let index = 0; index < liElement.length; index++) {
 //     console.log(liElement[index].textContent)
@@ -10,10 +10,10 @@ const liElement = document.getElementsByTagName("li");
 
 
 
-//Declaração de ARRAYS
+// Declaração de ARRAYS
 let nr1 = [1,2,3,4,5];
 let nr2 = [6,7,8,9,10];
-//let nr3 = nr1 + nr2;
+//Espalhar o array com SPREAD [ ... ]
 let nr3 = [nr1,nr2];
 
 console.log(`Array-1 ${nr1}`);
@@ -25,3 +25,21 @@ console.log(`Array-3 ${nr3}`);
 nr3.forEach((itemDoArray)=> {
     console.log(`Array-3 sendo listado ${itemDoArray}`);
 })  
+
+//Listando o array com MAP
+nr3.map((numero,key)=>{
+    console.log(`${(key+1)} - Itens do Array - ${numero}`);
+    numero.forEach((digitos)=>
+        console.log(digitos)
+    );
+})
+
+//Convertendo HTML collection em ARRAY
+
+const liElementHtml = document.getElementsByTagName("li");
+const liElementArray = [...liElementHtml]
+
+liElementArray.map((item,key)=>{
+
+    console.log(`${key+1} - ${item.textContent}`)
+})
