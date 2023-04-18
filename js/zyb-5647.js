b0a94e7955929221ad04d21352ecb7d653b4db48
GabrielMachadoCP <rm99880@fiap.com.br>
2023-04-18 11:31:26 -0300

    trbalhando com function, button e value

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -11,39 +11,48 @@
 
 
 // Declaração de ARRAYS
-let nr1 = [1,2,3,4,5];
-let nr2 = [6,7,8,9,10];
-//Espalhar o array com SPREAD [ ... ]
-let nr3 = [nr1,nr2];
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// //Espalhar o array com SPREAD [ ... ]
+// let nr3 = [nr1,nr2];
 
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+// console.log(`Array-1 ${nr1}`);
+// console.log(`Array-2 ${nr2}`);
+// console.log(`Array-3 ${nr3}`);
 
 
-//Listando um array! Técnica especial
-nr3.forEach((itemDoArray)=> {
-    console.log(`Array-3 sendo listado ${itemDoArray}`);
-})  
+// //Listando um array! Técnica especial
+// nr3.forEach((itemDoArray)=> {
+//     console.log(`Array-3 sendo listado ${itemDoArray}`);
+// })  
 
-//Listando o array com MAP
-nr3.map((numero,key)=>{
-    console.log(`${(key+1)} - Itens do Array - ${numero}`);
-    numero.forEach((digitos)=>
-        console.log(digitos)
-    );
-})
+// //Listando o array com MAP
+// nr3.map((numero,key)=>{
+//     console.log(`${(key+1)} - Itens do Array - ${numero}`);
+//     numero.forEach((digitos)=>
+//         console.log(digitos)
+//     );
+// })
 
 //Convertendo HTML collection em ARRAY
 
-const liElementHtml = document.getElementsByTagName("li");
-const liElementArray = [...liElementHtml]
+// const liElementHtml = document.getElementsByTagName("li");
+// const liElementArray = [...liElementHtml]
 
-liElementArray.map((item,key)=>{
+// liElementArray.map((item,key)=>{
 
-    if(item.textContent == "item-39"){
+//     if(item.textContent == "item-39"){
     
-        console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
-    }
-})
+//         console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
+//     }
+// })
 
+
+function soma(parametro) {
+    nr1 = 10;
+    nr2 = 5;
+    console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`);
+    console.log(`Este é o parâmetro que foi passado: ${parametro}`)
+}
+
+soma();
