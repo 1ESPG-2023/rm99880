84a7621a3ada9814b83a0a45d9c023a16a8d812b
GabrielMachadoCP <rm99880@fiap.com.br>
2023-05-02 11:45:59 -0300

    Adicionando os exercícios dos banners, lâmp + form

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,117 +1,158 @@
-// const liElement = document.getElementsByTagName("li");
+// // for (let index = 0; index < liElement.length ; index++) {
+// //     console.log(liElement[index].textContent)
+// // }
+// // // console.log(liElement.length)
 
-// for (let index = 0; index < liElement.length; index++) {
-//     console.log(liElement[index].textContent)
-    
-// }
-// console.log(liElement.length)
+// // //Declaração de arrays
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // // let nr3 = nr1+nr2;
+// // let nr3 = [nr1,nr2];
 
-//Objetos são formados por propriedades (innerHTML - formata tags) (innerText - formata textos)
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
 
+// // //Listando um array! Técnica especial
+// // // nr3.forEach((itemDoaArray)=>{
+// // //     console.log(`Array-3 sendo listado ${itemDoaArray}`);
+// // // })
+// // // Listando o array com MAP
+// // nr3.map((numero,key)=>{
+// //     console.log(`${(key+1)} - Itens do array - ${numero}`);
+// //     numero.forEach((digitos)=>{
+// //         console.log(digitos)
+// //     })
+// // })
 
+// // //Declaração de arrays
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // //Espalhar o array com SPREAD [ ... ]
+// // let nr3 = [...nr1,...nr2];
 
-// Declaração de ARRAYS
-// let nr1 = [1,2,3,4,5];
-// let nr2 = [6,7,8,9,10];
-// //Espalhar o array com SPREAD [ ... ]
-// let nr3 = [nr1,nr2];
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
 
-// console.log(`Array-1 ${nr1}`);
-// console.log(`Array-2 ${nr2}`);
-// console.log(`Array-3 ${nr3}`);
+// // nr3.map((numero,key)=>{
+// //     console.log(`${(key+1)} - Itens do array - ${numero}`);
 
+// // })
 
-// //Listando um array! Técnica especial
-// nr3.forEach((itemDoArray)=> {
-//     console.log(`Array-3 sendo listado ${itemDoArray}`);
-// })  
-
-// //Listando o array com MAP
-// nr3.map((numero,key)=>{
-//     console.log(`${(key+1)} - Itens do Array - ${numero}`);
-//     numero.forEach((digitos)=>
-//         console.log(digitos)
-//     );
-// })
-
-//Convertendo HTML collection em ARRAY
-
+// //Convertendo HTMLCollection em Array de forma indireta
 // const liElementHtml = document.getElementsByTagName("li");
-// const liElementArray = [...liElementHtml]
+// const liElementArray = [...liElementHtml];
 
 // liElementArray.map((item,key)=>{
-
-//     if(item.textContent == "item-39"){
-    
+//     if (item.textContent == "Item-39") {
 //         console.log(`${key+1} - ${item.textContent = "LOCALIZADO"}`)
 //     }
 // })
 
-
-
 // function soma(p1,p2,p3) {
 //     nr1 = p1;
 //     nr2 = p2;
-
-//     return (parseInt(nr1) + parseInt(nr2) + parseInt(p3))
-
-//     console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`);
-//     console.log(`Este é o parâmetro que foi passado: ${parametro}`)
+//     resultado = parseInt(nr1) + parseInt(nr2) + parseInt(p3)
+//     console.log(`O Resultado é : ${resultado}.`)
+//     return (resultado)
+//     // console.log(`Resultado : ${parseInt(nr1) + parseInt(nr2) + parseInt(parametro)}`);
+//     // console.log(`Este é o parâmetro que foi passado : ${parametro}`);
 // }
-
-
-
 
 // //ARROW FUNCTION
-// const mudaCor =  ()=>{
-
-//     //Função SET-TIME-OUT
-//     setTimeout(soma(1,2,3), 5000)
-
+// const mudaCor = ()=>{
+    
+//     //FUNÇÃO SET-TIME-OUT
+//     setTimeout(soma, 5000, 100,100,100);
 // }
 
-// mudaCor();
+// mudaCor()
 
-// let tmp = ""
+// let tmp = "";
 
 // function mudaCor() {
-
+    
 //     let r = "";
 //     let g = "";
 //     let b = "";
 
-
 //     //random = Gera números aleatórios entre 0 e 1
-//     //ceil, floor e round que arredondam o número para cima, para baixo ou aleatoriamente.
-    
-//     //Determinando para as variáveis r,g e b um valor entre 0 e 255.
+//     //ceil,floor e round que arredondam o número para cima, para baixo ou aleatóriamente.
+
+//     //Determinando para a variável r um valor entre 0 e 255.
 //     r = Math.round(Math.random() * 255);
 //     g = Math.round(Math.random() * 255);
 //     b = Math.round(Math.random() * 255);
 
-
 //     const cabecalho = document.querySelector(".cabecalho");
+    
+//     cabecalho.setAttribute("style", `background-color:rgb(${r},${g},${b})`)
 
-//     cabecalho.setAttribute("style", `background-color:rgb(${r},${g},${b})`);
+//     tmp = setTimeout(mudaCor, 1000);
 
-//     tmp = setTimeout(mudaCor, 150)
-
-//     console.log(r,g,b);
 // }
 
-// mudaCor();
 
-
-
-//COMEÇANDO O EXERCICIO
 //FUNÇÃO ALTERA BANNER
+// function alteraBanner() {
+    
+    
+//     const img1 = document.querySelector(".l-e > img")
+//     const img2 = document.querySelector(".l-d > img")
+    
+//     let nr = Math.ceil(Math.random() *3);
+//     let caminho = `./img/banner-lateral-${nr}.png`;
+//     img1.src = caminho;
+    
+//     nr = Math.ceil(Math.random() *3);
+//     caminho = `./img/banner-lateral-${nr}.png`;
+//     img2.src = caminho;
 
-function alteraBanner() {
+//     setTimeout(alteraBanner, 1000);
 
-    let nr = Math.round(Math.random() * 3);
+// }
 
-    let caminho = `./img/banner-lateral-${nr}.png`;
+// alteraBanner();
 
-    const img1 = document.querySelector(".l-e > img");
-    img1.src = caminho;
-}
+// const botao = document.querySelector(".central button");
+
+// botao.addEventListener("click" , ()=>{
+//     const imgLampada = document.querySelector(".central img");
+
+//     if(this.textContent == "LIGAR"){
+//         imgLampada.src = "./img/pic_bulbon.gif"
+//         botao.textContent = "DESLIGAR";
+//     }else{
+//         imgLampada.src = "./img/pic_bulboff.gif"
+//         botao.textContent = "LIGAR";
+//     }
+// });
+
+// function ligaDesliga() {
+
+//     const imgLampada = document.querySelector(".central img");
+
+//     if(botao.textContent == "LIGAR"){
+//         imgLampada.src = "./img/pic_bulbon.gif"
+//         botao.textContent = "DESLIGAR";
+//     }else{
+//         imgLampada.src = "./img/pic_bulboff.gif"
+//         botao.textContent = "LIGAR";
+//     }
+// }
+
+const inputUser = document.querySelector("input[name='nmUser']");
+
+inputUser.addEventListener("focus", ()=>{
+    inputUser.setAttribute("style","outline-color:#ff0000;");
+}); 
+
+inputUser.addEventListener("keyup", ()=>{
+    if(inputUser.value.length < 8){
+        inputUser.setAttribute("style", "outline-color:#ff0000");
+    }else{
+        inputUser.setAttribute("style", "outline-color:#dddddd");
+    }
+    
+});
