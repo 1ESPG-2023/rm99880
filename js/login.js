883fd38ac7bee72570fd1740371b35758f11b64f
GabrielMachadoCP <rm99880@fiap.com.br>
2023-05-09 11:55:29 -0300

    Fazendo lista de usuarios para validação de forms

--- js/login.js
+++ js/login.js
@@ -1,13 +1,28 @@
+//Gerando dois objetos no LOCALSTORAGE
+
+const usuario1 = {
+    nomeUsuario:"predo",
+    senhaUsuario:"1234"
+}
+
+const usuario2 = {
+    nomeUsuario:"anzina",
+    senhaUsuario:"12345"
+}
+
+let listaDeUsuarios = [];
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
+
+localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
+
+
 //Vamos criar um objeto para armazenar o nosso usuário
 
-//Estrutura básica de um objeto:
 // const usuario = {
-
+//     nomeUsuario:"",
+//     senhaUsuario:""
 // }
-const usuario = {
-    nomeUsuario:"",
-    senhaUsuario:""
-}
 
 addEventListener("click",(evento)=>{
 
@@ -17,18 +32,28 @@ addEventListener("click",(evento)=>{
         // let usuario = document.querySelector("#idUser").value;
         // let senha = document.querySelector("#idPass").value;
 
-        //Armazenando os dados no OBJETO
-        usuario.nomeUsuario = document.querySelector("#idUser").value;
-        usuario.senhaUsuario = document.querySelector("#idPass").value;
+        //Recuperando dos inputs
+        let inputUserValue = document.querySelector("#idUser").value;
+        let inputPassValue = document.querySelector("#idPass").value;
+
         const h1Titulo = document.querySelector("#titulo");
-        //VALIDAÇÃO
-        if(usuario.nomeUsuario == "rm99880" && usuario.senhaUsuario == "12345"){
-            console.log("VALIDADO!");
-            h1Titulo.innerHTML = "Bem vindo : " + usuario.nomeUsuario;
-        }else{
-            console.log("NÃO VALIDOU!");
-            h1Titulo.innerHTML = "";
-        }
+        
+        let lista = JSON.parse(localStorage.getItem("listaUser"));
+
+        lista.forEach((usuario)=>{
+            //VALIDAÇÃO
+            if(inputUserValue == usuario.nomeUsuario && inputPassValue == usuario.senhaUsuario){
+                console.log("VALIDADO!");
+                h1Titulo.innerHTML = "Bem vindo : " + usuario.nomeUsuario;
+                return;
+            }else{
+                console.log("NÃO VALIDOU!");
+                h1Titulo.innerHTML = "";
+                return;
+            }
+        });
+
+
 
 
     }
