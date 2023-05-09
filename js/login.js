c9410eef2fc0c03a2bb07a5d81b1b7780b2a8211
GabrielMachadoCP <rm99880@fiap.com.br>
2023-05-09 11:14:20 -0300

    Validando o formulário e adicionando titulos

--- js/login.js
+++ js/login.js
@@ -1,17 +1,36 @@
-addEventListener("click", (evento)=>{
-    //console.log(evento.target.id == "btnSubmit");
+//Vamos criar um objeto para armazenar o nosso usuário
+
+//Estrutura básica de um objeto:
+// const usuario = {
+
+// }
+const usuario = {
+    nomeUsuario:"",
+    senhaUsuario:""
+}
+
+addEventListener("click",(evento)=>{
 
     evento.preventDefault();
+
     if(evento.target.id == "btnSubmit"){
-        let usuario = document.querySelector("#idUser").value;
-        let senha = document.querySelector("#idPass").value;
-        console.log(usuario,senha);
-        
+        // let usuario = document.querySelector("#idUser").value;
+        // let senha = document.querySelector("#idPass").value;
+
+        //Armazenando os dados no OBJETO
+        usuario.nomeUsuario = document.querySelector("#idUser").value;
+        usuario.senhaUsuario = document.querySelector("#idPass").value;
+        const h1Titulo = document.querySelector("#titulo");
         //VALIDAÇÃO
-        if(usuario == "rm99880" && senha == "12345"){
-            console.log("VALIDADO!")
+        if(usuario.nomeUsuario == "rm99880" && usuario.senhaUsuario == "12345"){
+            console.log("VALIDADO!");
+            h1Titulo.innerHTML = "Bem vindo : " + usuario.nomeUsuario;
         }else{
             console.log("NÃO VALIDOU!");
+            h1Titulo.innerHTML = "";
         }
+
+
     }
+
 });
