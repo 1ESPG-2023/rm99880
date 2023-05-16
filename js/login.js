c8f6cbf928aabaefded9f8e2218712f9eb4fac7d
GabrielMachadoCP <rm99880@fiap.com.br>
2023-05-16 11:50:00 -0300

    Adicionando páginas de sucesso e erro para validar

--- js/login.js
+++ js/login.js
@@ -40,12 +40,15 @@ addEventListener("click",(evento)=>{
         const h1Titulo = document.querySelector("#titulo");
 
         let lista = JSON.parse(localStorage.getItem("listaUser"));
+
+        let userValidado = {};
         
         try{
             lista.forEach((usuario)=> {
                 //VALIDAÇÃO
                 if(inputUserValue == usuario.nomeUsuario && inputPassValue == usuario.senhaUsuario){
-                throw "VALIDADO";
+                    userValidado = usuario;
+                    throw "VALIDADO";
                 }
             });
 
@@ -53,14 +56,47 @@ addEventListener("click",(evento)=>{
 
         }catch(msg){
             if(msg == "VALIDADO"){
-                console.log("VALIDADO");
-                h1Titulo.innerHTML = "<span><strong>Login validado com sucesso!</strong></span>"
-                h1Titulo.setAttribute("style", "color: #00ff00;")
+                h1Titulo.innerHTML = "<span><strong>Login validado com sucesso!</strong></span>";
+                h1Titulo.setAttribute("style","color:#00ff00;");
+
+                //Adicionando uma propriedade ao nosso objeto userValidado
+                userValidado["token"] = Math.random().toString(16).substring(2)+Math.random().toString(16).substring(2);
+
+                //Setando um novo objeto no LocalStorage
+                localStorage.setItem("UserValidado",  JSON.stringify(userValidado));
+                //Direcionando o usuário para a página de sucesso!
+                window.location.href = "../sucesso.html";
+
             }else{
-                console.log("NÃO VALIDADO");
-                h1Titulo.innerHTML = "<span><strong>Login ou senha inválidos!</strong></span>"
-                h1Titulo.setAttribute("style", "color: #ff0000;")
+                h1Titulo.innerHTML = "<span><strong>Login ou senha inválidos!</strong></span>";
+                h1Titulo.setAttribute("style","color:#ff0000;");
+                window.location.href = "../erro.html";
             }
         }       
     }
 });
+
+try{
+    const userBemVindo = document.querySelector("#userWelcome");
+    let usuario = JSON.parse(localStorage.getItem("UserValidado"));
+    if(usuario){
+        
+        if(usuario.token){
+            userBemVindo.innerHTML = usuario.nomeUsuario;
+        }
+
+    }else{
+        window.location.href = "../erro.html";
+    }
+
+    const botaoLogout = document.querySelector("#btnLogout")
+    botaoLogout.addEventListener("click", ()=>{
+        localStorage.removeItem("userValidado")
+        window.location.href = "../login.html";
+    });
+}catch(erro){
+
+    if(userBemVindo != null){
+        userBemVindo.innerHTML = JSON.parse(localStorage.getItem("UserValidado")).nomeUsuario;
+    }
+}
